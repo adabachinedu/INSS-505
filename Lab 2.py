8ccf72228cf9ead6f1ac57c18b95362633b0f789
austinadaba316 <adabac1019@students.bowiestate.edu>
2023-09-23 21:53:21 -0400

    commit for my lab2 update

---
+++ Lab 2.py
@@ -0,0 +1,4 @@
+print('My name is Chinedu Adaba')
+print('I am a graduate student in Management Information System')
+print('I am the last child in my family, I love soccer and I played basketball')
+print("hello, how are you")
