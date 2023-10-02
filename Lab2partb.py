5c78963c247c6504ec16b0f78316462d0926080a
austinadaba316 <adabac1019@students.bowiestate.edu>
2023-10-02 17:54:28 -0400

    commit for my lab2 update

---
+++ Lab2partb.py
@@ -0,0 +1,40 @@
+#Definition of variables
+number1=10
+number2=11
+number3=12
+number4=13
+#step 3
+add=number1+number2+number3+number4
+mult=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+#step 4
+print(add)
+print(mult)
+print(subtract)
+print(division)
+print(add, mult, subtract, division)
+number1=13
+number2=12
+number3=11
+number4=10
+#step 6
+add=number1+number2+number3+number4
+mult=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+print(add)
+print(mult)
+print(subtract)
+print(division)
+print(add, mult, subtract, division)
+#step8
+add=number1+number2
+multi=number3*number4
+subtract=number1-number2
+division=number1/number2
+print(add)
+print(multi)
+print(subtract)
+print(division)
+print(add, multi, subtract, division)
